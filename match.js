887959caf9767bfd9ff2fa6752258275d5668f5d
benespo <benespo@live.unc.edu>
2019-11-24 19:02:10 -0500

    new

--- match.js
+++ match.js
@@ -2,7 +2,7 @@
 /*Each MC question will have a percentage value associated with its importance along with a percentage 
   value for each associated question
      - answers are also given a score for thier relationship to the matched answer 
-     -  Close answers are given .7 or .5 of the total point value, further answers can be given .2 
+     -  Close answers are given .5 of the total point value, further answers can be given 0
         all others are given 0
 */
 
@@ -18,13 +18,18 @@ function getResults(){
 }
 
 function scoreAndCompare(){
+
+    //Throw out missing answers if someone forgets
+
     /*MC:
     score values will be stored based on their relationship to the corect answer
     if (patient answers = provider answers + 1 * pointvalue for that question)
     else(Check to see if your answer is within range of getting points) { credit ginven * point value for that quesiton}*/
 
-    //Check Box: matched boxes checked from paitients/total boxes checked by the provider (for each question)
-
+    //Check Box: matched boxes checked from paitients/ total number of quesitons in check box (for each question)
+    // subtract for each mismatch 
+    
+    
     //Slider will check how close the value is to the health provider's and multiply that by the point value awarded for that question
 
 }
