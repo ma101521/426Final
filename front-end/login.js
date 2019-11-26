9f834c3c16fabc868fd8c441ae1fd1e57af0997f
Marisa <ma101521@live.unc.edu>
2019-11-26 14:05:34 -0500

    using jwt, but not storing it yet

--- front-end/login.js
+++ front-end/login.js
@@ -11,18 +11,23 @@ $(function() {
         console.log($('#username').val());
         console.log($('#password').val());
 
-        let email = $('#username').val();
+        let username = $('#username').val();
         let password = $('#password').val();
 
         axios.post('http://localhost:3000/account/create',
         {
-          name: email,
+          name: username,
           pass: password
         })
         .then(response => {
-          console.log(response);
+          console.log(response.status);
+          if(response.status === 200){
+            $('#errorBanner').empty()
+            window.location.href = "survey.html";
+          }
         }).catch(error => {
           console.log(error);
+          $('#errorBanner').append(`<p>${error}</p>`)
         });
     }
 
@@ -31,18 +36,29 @@ $(function() {
         console.log($('#username').val());
         console.log($('#password').val());
 
-        let email = $('#username').val();
+        let username = $('#username').val();
         let password = $('#password').val();
 
         axios.post('http://localhost:3000/account/login',
         {
-          name: email,
+          name: username,
           pass: password
         })
         .then(response => {
           console.log(response.status);
           if(response.status === 200){
-            window.location.href = "dashboard.html";
+            console.log(response.data.jwt)
+            let jwt = response.data.jwt;
+            axios.get('http://localhost:3000/account/status',
+            {
+              headers: {
+                //jwt is the jwt from logging in
+                "Authorization": "Bearer " + jwt
+              },
+            })
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+            //window.location.href = "dashboard.html";
           }
         }).catch(error => {
           console.log(error);
