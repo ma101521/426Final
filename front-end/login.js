787d16d53ad1846c8c28595cf6c7422860991ee6
Marisa <ma101521@live.unc.edu>
2019-11-26 10:09:12 -0500

    working on login

--- front-end/login.js
+++ front-end/login.js
@@ -3,16 +3,15 @@
 
 $(function() {
     //test loading
-    $('.signupButton').click(logSignUp);
+    $('.signupButton').click(SignUp);
+    $('.loginButton').click(Login);
 
-    function logSignUp(event){
+    function SignUp(event){
         event.preventDefault();
-        console.log("hi");
-        console.log(event.currentTarget);
-        console.log($('#email').val());
+        console.log($('#username').val());
         console.log($('#password').val());
 
-        let email = $('#email').val();
+        let email = $('#username').val();
         let password = $('#password').val();
 
         axios.post('http://localhost:3000/account/create',
@@ -25,5 +24,25 @@ $(function() {
         }).catch(error => {
           console.log(error);
         });
+    }
+
+    function Login(event){
+      event.preventDefault();
+        console.log($('#username').val());
+        console.log($('#password').val());
+
+        let email = $('#username').val();
+        let password = $('#password').val();
+
+        axios.post('http://localhost:3000/account/login',
+        {
+          name: email,
+          pass: password
+        })
+        .then(response => {
+          console.log(response);
+        }).catch(error => {
+          console.log(error);
+        });  
     }
 });
