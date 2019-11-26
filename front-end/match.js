c357d13ea9b32bdfca296a3e0d666ce0b020ebe5
benespo <benespo@live.unc.edu>
2019-11-25 19:37:50 -0500

    moved folder

--- front-end/match.js
+++ front-end/match.js
@@ -11,15 +11,18 @@ function getResults(){
     let answers = []
     answers[0] = patient["I am a"];
     answers[1] = patient["What is your gender?"];
-    //Storing info from a check box with multiple answers
-    //Do we want to use a list?  
+    //Storing info from a check box with multiple answers  
     answers[2] = patient["Which of the following statements describe you. Please check all that apply."];
 
 }
 
+//Survey Java script value (based on login)
+//How are checkboxes and radio buttons stored??
+//Attach each question with a console.log() to see how the data is being stored
+
 function scoreAndCompare(){
 
-    //Throw out missing answers if someone forgets
+    //Make sure no one misses a quesiton
 
     /*MC:
     score values will be stored based on their relationship to the corect answer
@@ -27,10 +30,11 @@ function scoreAndCompare(){
     else(Check to see if your answer is within range of getting points) { credit ginven * point value for that quesiton}*/
 
     //Check Box: matched boxes checked from paitients/ total number of quesitons in check box (for each question)
-    // subtract for each mismatch 
+    //count the total number that are the same even the omited question: treat each check box question like a mini survey
     
     
     //Slider will check how close the value is to the health provider's and multiply that by the point value awarded for that question
+    //Have ranges 
 
 }
 
