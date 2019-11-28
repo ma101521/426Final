30fd392ccda0490f74a07a64d697d44f5b14c416
benespo <benespo@live.unc.edu>
2019-11-27 22:57:40 -0500

    algorithm-basics

--- front-end/match.js
+++ front-end/match.js
@@ -7,27 +7,55 @@
 */
 
 function getResults(){
-    let patient = users.find(user => username = user1)
-    let answers = []
-    answers[0] = patient["I am a"];
-    answers[1] = patient["What is your gender?"];
-    //Storing info from a check box with multiple answers  
-    answers[2] = patient["Which of the following statements describe you. Please check all that apply."];
+    $(document).ready(function(){
+        $("form#Survey").submit(function(event){
+          event.preventDefault();
+        //figure out how to store these values in the object key? do we want to heirarchal key like in a03?
+          $("input:checkbox[name=Which of the following statements describe you. Please check all that apply.]:checked").each(function(){
+            var workTransportationMode = $(this).val();
+        });
 
-}
+        //figure out how to store these values in the object key? do we want to heirarchal key like in a03?
+        $("input:checkbox[name=Which of the following statements describe you. Please check all that apply.]:checked").each(function(){
+            var workTransportationMode = $(this).val();
+        });
+    });
+});
 
 //Survey Java script value (based on login)
 //How are checkboxes and radio buttons stored??
 //Attach each question with a console.log() to see how the data is being stored
 
 function scoreAndCompare(){
-
     //Make sure no one misses a quesiton
+    let count = 0; 
+
+    let patient = {}
+    //Retrive patient object
+
+    let provider = {}
+    //Retrive patient object
 
     /*MC:
     score values will be stored based on their relationship to the corect answer
     if (patient answers = provider answers + 1 * pointvalue for that question)
     else(Check to see if your answer is within range of getting points) { credit ginven * point value for that quesiton}*/
+
+    if(patient['What is your gender?'] == provider['What is your gender?']){
+        count++;
+    }
+
+    //Is there a way to traverse through these? 
+    if(patient['I like routine.'] == provider['I like routine.']){
+        count++;
+    }
+    else if(patient['I like routine.'] == provider['I like routine.']-1 || patient['I like routine.'] == provider['I like routine.']+1){
+        count = count + .5; 
+    }
+
+    
+
+
 
     //Check Box: matched boxes checked from paitients/ total number of quesitons in check box (for each question)
     //count the total number that are the same even the omited question: treat each check box question like a mini survey
@@ -38,6 +66,6 @@ function scoreAndCompare(){
 
 }
 
-function calculateMatchPercentage(){
-    //We will put the percentages in some sor of accumulator and get the resuls here 
+function calculateMatchPercentage(count){
+    //We will put the percentages in some sor of accumulator and get the resuls here
 }
