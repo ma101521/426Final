92f881faa1b1715cae21a111c270e92aa755c1fd
Marisa <ma101521@live.unc.edu>
2019-12-02 18:59:46 -0500

    testing create

---
+++ front-end/test.js
@@ -0,0 +1,31 @@
+
+$(function() {
+
+    $('#submitbutton').click(create);
+
+
+    function create(event){
+        event.preventDefault();
+        console.log('hi');
+        let type = $('input[name="I am a"]:checked').val();
+        if(type == 'patient'){
+            axios.post('http://localhost:3000/patient/create',
+            {
+            name: 'username',
+            pass: 'password'
+            })
+            .then(response=> console.log(reponse))
+            .catch(error=> console.log(error))
+        }
+        else{
+            axios.post('http://localhost:3000/provider/create',
+            {
+            data: {
+                body: 'username'
+            }
+            })
+            .then(response=> console.log(reponse))
+            .catch(error=> console.log(error))
+        }
+    }
+})
