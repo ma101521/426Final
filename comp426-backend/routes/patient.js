9c989611654dd8b4d8752507364b2bf30fb55384
jillianaraya <jillianaraya@users.noreply.github.com>
2019-12-02 18:24:29 -0500

    updateA

--- comp426-backend/routes/patient.js
+++ comp426-backend/routes/patient.js
@@ -1,14 +1,15 @@
 import express from "express";
-import {authenticateUser} from "../middlewares/auth";
+import { authenticateUser } from "../middlewares/auth";
 import jwt from 'jsonwebtoken';
 
 export const router = express.Router();
 export const prefix = '/patient';
 
 
-const {patientStore} = require('../data/DataStore');
+const { patientStore } = require('../data/DataStore');
 
 //method to post patient
+
 //router.post('/create', async function (req, res) {}
 
 //method to get patient with username input
