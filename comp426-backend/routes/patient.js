71907e4411d5ec1416292ceb37c8a3c375980c93
danarubin2 <dana.r.r25@gmail.com>
2019-12-03 02:16:23 -0500

    patient route has values for questions now

--- comp426-backend/routes/patient.js
+++ comp426-backend/routes/patient.js
@@ -18,27 +18,38 @@ const { patientStore } = require('../data/DataStore');
 //router.post('/create', async function (req, res) {}
 
 router.post('/create', async function (req, res) {
-    if (!req.body.name || !req.body.pass) {
-        res.status(401).send({ msg: 'Expected a payload of name and pass.' });
+    if (!req.body.name) {
+        res.status(401).send({ msg: 'Expected a payload of name.' });
         return;
     }
-
     const name = req.body.name.toLowerCase();
-    const pass = req.body.pass;
+    //const pass = req.body.pass;
 
-    let user = patientStore.get(`users.${name}`);
-    if (user) {
-        res.status(401).send({ msg: `User '${req.body.name}' is already a registered user.` });
-        return;
-    }
-
-    bcrypt.hash(pass, saltRounds, (err, hash) => {
+    // let user = patientStore.get(`users.${name}`);
+    // if (user) {
+    //     res.status(401).send({ msg: `User '${req.body.name}' is already a registered user.` });
+    //     return;
+    // }
+    // bcrypt.hash(pass, saltRounds, (err, hash) => {
         patientStore.set(`users.${name}`, {
-            passwordHash: hash,
-            data: req.body.data
+            //passwordHash: hash,
+            data: req.body.data,
+            q1: req.body.q1,
+            q2: req.body.q2,
+            q3: req.body.q3,
+            q4: req.body.q4,
+            q5: req.body.q5,
+            q6: req.body.q6,
+            q7: req.body.q7,
+            q8: req.body.q8,
+            q9: req.body.q9,
+            q10: req.body.q10,
+            q11: req.body.q11,
+            q12: req.body.q12
         });
+        console.log(req);
         res.send({ data: userFilter(patientStore.get(`users.${name}`)), status: 'Successfully made account' });
-    });
+    // });
 });
 
 
