647b9a4bfbb935c30c17ae12ddfac6c1b4597535
benespo <benespo@live.unc.edu>
2019-12-04 18:26:35 -0500

    match comments

--- front-end/match.js
+++ front-end/match.js
@@ -6,66 +6,54 @@
         all others are given 0
 */
 
-function getResults(){
-    $(document).ready(function(){
-        $("form#Survey").submit(function(event){
-          event.preventDefault();
-        //figure out how to store these values in the object key? do we want to heirarchal key like in a03?
-          $("input:checkbox[name=Which of the following statements describe you. Please check all that apply.]:checked").each(function(){
-            var workTransportationMode = $(this).val();
-        });
-
-        //figure out how to store these values in the object key? do we want to heirarchal key like in a03?
-        $("input:checkbox[name=Which of the following statements describe you. Please check all that apply.]:checked").each(function(){
-            var workTransportationMode = $(this).val();
-        });
-    });
-});
-
-//Survey Java script value (based on login)
-//How are checkboxes and radio buttons stored??
-//Attach each question with a console.log() to see how the data is being stored
-
-function scoreAndCompare(){
-    //Make sure no one misses a quesiton
-    let count = 0; 
-
-    let patient = {}
-    //Retrive patient object
-
-    let provider = {}
-    //Retrive patient object
-
-    /*MC:
-    score values will be stored based on their relationship to the corect answer
-    if (patient answers = provider answers + 1 * pointvalue for that question)
-    else(Check to see if your answer is within range of getting points) { credit ginven * point value for that quesiton}*/
-
-    if(patient['What is your gender?'] == provider['What is your gender?']){
-        count++;
-    }
-
-    //Is there a way to traverse through these? 
-    if(patient['I like routine.'] == provider['I like routine.']){
-        count++;
-    }
-    else if(patient['I like routine.'] == provider['I like routine.']-1 || patient['I like routine.'] == provider['I like routine.']+1){
-        count = count + .5; 
-    }
-
-    
+/*+++++++++++++++POINT VALUES++++++++++++++++++
+                    q1 : 1
+                    q2 : 1
+                    q3 : 6
+                    q4 : 1
+                    q5 : 1
+                    q6 : 1
+                    q7 : 1
+                    q8 : 1
+                    q9 : 1
+                    q10: 3
+                    q11: 3
+*/
 
 
+//Survey Java script value (based on login)
+//How are checkboxes and radio buttons stored??
+//Attach each question with a console.log() to see how the data is being stored
+//patient and provoider as parameters will not need to get results in function 
 
-    //Check Box: matched boxes checked from paitients/ total number of quesitons in check box (for each question)
-    //count the total number that are the same even the omited question: treat each check box question like a mini survey
-    
-    
-    //Slider will check how close the value is to the health provider's and multiply that by the point value awarded for that question
-    //Have ranges 
-
+function scoreAndCompare(patient, provider){
+        //Make sure no one misses a quesiton
+        let count = 0; 
+        let patient = {}
+        //Retrive patient object
+        let provider = {}
+        //Retrive patient object
+        /*MC:
+        score values will be stored based on their relationship to the corect answer
+        if (patient answers = provider answers + 1 * pointvalue for that question)
+        else(Check to see if your answer is within range of getting points) { credit ginven * point value for that quesiton}*/
+        
+        //Check Box: matched boxes checked from paitients/ total number of quesitons in check box (for each question)
+        //count the total number that are the same even the omited question: treat each check box question like a mini survey
+        
+        
+        
+        //Slider will check how close the value is to the health provider's and multiply that by the point value awarded for that question
+        //Have ranges
+        
+        // Difference between the numbers 
+        // If possitve subtract if negative add 
+        // x = 10 - (or + if negative) difference
+        // x * (question value)/10 = points awarded 
 }
-
-function calculateMatchPercentage(count){
-    //We will put the percentages in some sor of accumulator and get the resuls here
+    
+function calculateMatchPercentage(pointCount){
+        //We will put the percentages in some sor of accumulator and get the resuls here
+    return pointcount/20;
 }
+    
