f5a580070516be5b9827200ba411f7cebdcbb7cd
Marisa <ma101521@live.unc.edu>
2019-12-03 19:49:08 -0500

    buttons

--- front-end/dashboard.js
+++ front-end/dashboard.js
@@ -1,3 +1,23 @@
 $(function(){
 
+    $('#getUser').click(getUser);
+    $('#getProviders').click(getAllProviders);
+
+    function getUser(event){
+        event.preventDefault();
+        console.log('work');
+        let username = $('#username').val();
+        axios.get('http://localhost:3000/patient/'+username,)
+        .then(response=> console.log(response.data.data))
+        .catch(error => console.log(error.response))
+    }
+
+    function getAllProviders(event) {
+        event.preventDefault();
+        console.log('hellooooo');
+        //let username = $('#username').val();
+        axios.get('http://localhost:3000/provider/all')
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error.response))
+    }
 })
