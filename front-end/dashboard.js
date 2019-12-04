93ecc1bf262ae2776e4e64bace96303ddd43b21d
Marisa <ma101521@live.unc.edu>
2019-12-04 17:33:26 -0500

    adding map

--- front-end/dashboard.js
+++ front-end/dashboard.js
@@ -1,20 +1,27 @@
 $(function(){
 
-    $('#getUser').click(getUser);
-    $('#getProviders').click(getAllProviders);
+    let jwt = localStorage.getItem('jwt');
+    /* $('#getUser').click(getUser);
+    $('#getProviders').click(getAllProviders); */
+    getUser();
+    getAllProviders();
 
-    function getUser(event){
-        event.preventDefault();
-        console.log('work');
-        let username = $('#username').val();
-        axios.get('http://localhost:3000/patient/'+username,)
-        .then(response=> console.log(response.data.data))
-        .catch(error => console.log(error.response))
+    function getUser(event) {
+        //event.preventDefault();
+        axios.get('http://localhost:3000/patient/user',
+        {
+            headers: {
+              //jwt is the jwt from logging in
+              "Authorization": "Bearer " + jwt
+            },
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error.reponse))
     }
 
     function getAllProviders(event) {
-        event.preventDefault();
-        console.log('hellooooo');
+        //event.preventDefault();
+        //console.log('hellooooo');
         //let username = $('#username').val();
         axios.get('http://localhost:3000/provider/all')
             .then(response => console.log(response.data))
